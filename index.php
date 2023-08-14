20dc7f064e56ada716eb8f3c88faa7967af8534e
arif rizki <arifwise67@gmail.com>
2023-08-14 21:27:50 +0800

    menambah paragraph

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis saepe optio, veniam dignissimos similique molestiae eum temporibus quasi repellat totam odit. Iure, nisi delectus. Dolores illum harum enim ut deleniti?</p>
 </body>
 </html>
